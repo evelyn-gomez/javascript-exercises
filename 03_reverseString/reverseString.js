60870e979e2cbfafe3c6c62a88fa2ea9e9094ce8
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-09-16 13:22:25 -0500

    exercises 03_reverseString`

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function(str) {
+const reverseString = function(str){
+    let reversedStr =''; 
+    for (let i = str.length ; i > -1 ; i--){
+        let charAtStr = str.charAt(i); 
+        reversedStr += charAtStr;  
+    }
+    return reversedStr; 
+}
+//  other Option is 
+    // function (str){
+    //     return reversed = str.split('').reverse().join('')  
+    // }
 
-};
 
 // Do not edit below this line
 module.exports = reverseString;
