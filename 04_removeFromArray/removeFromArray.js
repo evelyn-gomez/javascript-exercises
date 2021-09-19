1458431555b3d6a9ebf0d7687c5929938037c233
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-09-18 19:16:39 -0500

    Updated removeArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,5 @@
 const removeFromArray = function(array,...excludeArgs) {
-    let excludeArray = Array.from(excludeArgs); 
-    // let numsToExclude = excludeArray.filter(item => !isNaN(item))
-    return array.filter(item => !excludeArray.includes(item)); 
+    return array.filter(item => !excludeArgs.includes(item)); 
 };  
 // Do not edit below this line
 module.exports = removeFromArray;
