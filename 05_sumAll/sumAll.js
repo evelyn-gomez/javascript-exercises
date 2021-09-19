49b8e42d089ee9417286b77faab6711dfe1f409c
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-09-18 19:22:39 -0500

    exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let allNums = []; 
 
+    if((!Number.isInteger(a)) || (!Number.isInteger(b))){
+        return 'ERROR'
+    }else if (a < 0 || b < 0){
+        return 'ERROR'
+    } else {
+        if (a < b){
+            for(let i=a; i >=a && i <= b; i++){
+                allNums.push(i);
+            };     
+        } else {
+            for(let i=b; i <= a && i >= b; i++){
+                allNums.push(i);
+            };  
+        }
+    }
+    let addingNums = (prevVal, currentVal) =>
+        prevVal + currentVal; 
+    return allNums.reduce(addingNums);
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
