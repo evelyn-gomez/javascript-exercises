339e0ee9b957af80b9613858d57c536a8acf2edc
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-12-04 18:31:53 -0800

    my solution for problems  minus factorial check solution via stackoverflow

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = array.reduce((prev,current)=>{
+    return prev + current; 
+    
+  },0);
+  return parseInt(sum); 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array.reduce((prev,current) => {
+    return prev * current; 
+  })
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b); 
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+  if(num === 0 || num === 1 ){
+    return 1; 
+  }else if(num >1){
+    return (num * factorial(num -1));  
+  }
 };
 
+factorial(5);
 // Do not edit below this line
 module.exports = {
   add,
