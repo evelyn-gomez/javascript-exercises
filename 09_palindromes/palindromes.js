299700ffa1a8c81e10dc28d3c3aa377c8cb89118
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-12-09 10:53:42 -0800

    Took longer than expect, to actually code it was about 15 min but 8 hours probably

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+function removeNonLetters(str){
+  let arrayOfStr = str.split(''); 
+  let newString = arrayOfStr
+                          .filter(element =>  element !== ',' && element !== '.' && element !== '!')
+                          .join('')
+                          .toString()
+                          .replace(/\s+/g, '')
+  return newString; 
+}
 
+
+const palindromes = function (str) {
+  let string = removeNonLetters(str); 
+  let reversedString = [...string]
+                                  .reverse()
+                                  .join(''); 
+  if (string.toLowerCase() === reversedString.toLowerCase()){
+    return true; 
+  }
+  return false;
 };
 
+// console.log(palindromes('racecar')); 
 // Do not edit below this line
 module.exports = palindromes;
