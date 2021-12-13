7e7128e55dcaa3d06e29677881fcb9c138a2ded1
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-12-12 17:12:03 -0800

    not that great of solution but passes test suite

--- 10_fibonacci/README.md
+++ 10_fibonacci/README.md
@@ -8,3 +8,7 @@ Create a function that returns a specific member of the Fibonacci sequence:
 fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
 fibonacci(6) // returns 8
 ```
+## Reflection 
+  My initial thought is I could use array.reduce() 
+  I still think think it be more cleaner if I go that route, but since it did take me a bit longer than expected, I decided to
+  just keep my current solution.
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
 
+const fibonacci = function(num) {
+  let n = parseInt(num);
+  if( n < 0){
+    return 'OOPS';
+  } else if( n ===1){
+    return 1; 
+  }
+  let array =[0,1];
+  let current; 
+  for(let i=1; i < n; i++){ 
+    current = array[i]; 
+    let prev = array[i-1]; 
+    current += prev;
+    array.push(current); 
+  }
+  return current; 
 };
+
+console.log(fibonacci('1'));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
