89f981582e2c1fafeb0162d6b08aba224e9aad0c
Evelyn Gomez <evelyn.gomez15@gmail.com>
2021-12-18 11:51:49 -0800

    Added comments to README.md file

--- 12_findTheOldest/README.md
+++ 12_findTheOldest/README.md
@@ -6,3 +6,24 @@ Given an array of objects representing people with a birth and death year, retur
 - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
 - this can be done with a couple of chained array methods, or by using `reduce`.
 - One of the tests checks for people with no death-date.. use JavaScript's Date function to get their age as of today.
+
+##
+ My current solution was not the best but it worked for me. I review the solution for this on TOP and found that using the .reduce() method would have yieled a cleaner result. I did try a few times using it but couldn't figure out how to make it work for me. 
+
+ I am also adding another solution that my mentor went over with me on here 
+```
+Javascript 
+function findTheOldest2(people) {
+  let oldest = people[0];
+  let maxAge = getAge(oldest);
+  for (let person of people) {
+    let age = getAge(person);
+    if (age > maxAge) {
+      maxAge = age;
+      oldest = person;
+    }
+  } 
+  return person;
+}
+```
+This was much cleaner imo than using ```reduce()``` method. 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,68 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+function getAge(element){
+  let lastYear = parseInt(element.yearOfDeath);
+  if (isNaN(lastYear)) {
+    lastYear = new Date().getFullYear()
+  }
+  return lastYear - element.yearOfBirth
+}
 
+function getOldest(obj){
+ let arrayOfAges = obj  
+      .map(element => getAge(element))
+      .sort();
+  return arrayOfAges[arrayOfAges.length -1];
 };
+
+const findTheOldest = function(arr) {
+  let maxAge = getOldest(arr);
+  for(let i = 0; i < arr.length; i++){
+    let age = getAge(arr[i]);
+    if(age === maxAge){
+      return arr[i]; 
+    }
+  } 
+}
+
+/** @typedef {{ name: string, yearOfBirth: number, yearOfDeath: number | undefined }} Person */
+
+/**
+ * 
+ * @param {Person[]} people 
+ * @returns {string}
+ */
+function findTheOldest2(people) {
+  let oldest = people[0];
+  let maxAge = getAge(oldest);
+  for (let person of people) {
+    let age = getAge(person);
+    if (age > maxAge) {
+      maxAge = age;
+      oldest = person;
+    }
+  } 
+  return person;
+}
+console.log(getOldest(people));
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
